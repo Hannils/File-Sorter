ba531a0afcd8d04a590b643cfb38acea33a71018
Hannils <15hannils@gmail.com>
2024-08-17 20:59:24 +0200

    Added option to clean all current files once or keep cleaning future files until terminated

--- main.py
+++ main.py
@@ -8,6 +8,8 @@ from watchdog.events import FileSystemEventHandler
 # Configure logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+directory = 'PATH_TO_DIRECTORY'
+
 # Define file types and target directories
 file_types = {
     ".jpeg": "Pictures",
@@ -31,20 +33,49 @@ file_types = {
     ".torrent": "Torrents"
 }
 
+def cleaner(directory):
+    logging.info('Starting cleanup of existing files...')
+    for file_name in os.listdir(directory):
+        file_path = os.path.join(directory, file_name)
+        
+        # Skip directories
+        if os.path.isdir(file_path):
+            continue
+        
+        file_ext = os.path.splitext(file_name)[1]
+        dest_dir = file_types.get(file_ext, 'Unsorted')
+        dest_path = os.path.join(directory, dest_dir)
+        
+        if not os.path.exists(dest_path):
+            os.makedirs(dest_path)
+        
+        dest_file_path = os.path.join(dest_path, file_name)
+
+        # Handle file name conflicts
+        base, ext = os.path.splitext(file_name)
+        counter = 1
+        while os.path.exists(dest_file_path):
+            dest_file_path = os.path.join(dest_path, f"{base}_{counter}{ext}")
+            counter += 1
+        
+        try:
+            shutil.move(file_path, dest_file_path)
+            logging.info(f'Moved {file_name} to {dest_dir}')
+        except Exception as e:
+            logging.error(f'Error moving {file_name}: {e}')
+
 class MyHandler(FileSystemEventHandler):
     def on_created(self, event):
         if not event.is_directory:
             file_path = event.src_path
             file_name = os.path.basename(file_path)
             file_ext = os.path.splitext(file_name)[1]
-            dest_dir = file_types.get(file_ext)
-            if not dest_dir:
-                dest_dir = 'Unsorted'
-            dest_path = os.path.join(os.path.expanduser('DOWNLOADS_PATH'), dest_dir)
+            dest_dir = file_types.get(file_ext, 'Unsorted')
+            dest_path = os.path.join(directory, dest_dir)
             if not os.path.exists(dest_path):
                 os.makedirs(dest_path)
             dest_file_path = os.path.join(dest_path, file_name)
-            
+
             # Handle file name conflicts
             base, ext = os.path.splitext(file_name)
             counter = 1
@@ -56,15 +87,28 @@ class MyHandler(FileSystemEventHandler):
                 logging.info(f'Moved {file_name} to {dest_dir}')
             except Exception as e:
                 logging.error(f'Error moving {file_name}: {e}')
-if __name__ == "__main__":
-    event_handler = MyHandler()
-    observer = Observer()
-    observer.schedule(event_handler, path=os.path.expanduser('DOWNLOADS_PATH'), recursive=False)
-    observer.start()
 
-    try:
-        while True:
-            time.sleep(1)
-    except KeyboardInterrupt:
-        observer.stop()
-    observer.join()
+if __name__ == "__main__":
+    print(" _____  __           ____             _             ")
+    print("|  ___(_) | ___     / ___|  ___  _ __| |_ ___ _ __ ")
+    print("| |_  | | |/ _ \\____\\___ \\ / _ \\| '__| __/ _ \\ '__|")
+    print("|  _| | | |  __/_____|__) | (_) | |  | ||  __/ |   ")
+    print("|_|   |_|_|\\___|    |____/ \\___/|_|   \\__\\___|_|   ")
+    print()
+    print()
+    val = input("Do you want to clean up current (Cleans up all current files once) or future (Cleans up all future files until terminated)? [once/future]: ")
+    if val == 'once':
+        cleaner(directory)
+    elif val == 'future':
+        event_handler = MyHandler()
+        observer = Observer()
+        observer.schedule(event_handler, path=directory, recursive=False)
+        observer.start()
+        try:
+            while True:
+                time.sleep(1)
+        except KeyboardInterrupt:
+            observer.stop()
+        observer.join()
+    else:
+        print("Shutting down")
